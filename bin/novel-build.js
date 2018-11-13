5b059ef746f4016a3fce29cab07029a59ed61a33
8amjp <wakeup@8am.jp>
2018-11-13 20:46:40 +0900

    0.3.5

--- bin/novel-build.js
+++ bin/novel-build.js
@@ -7,7 +7,6 @@ const Publisher = require('../lib/Publisher.js');
 const Narou = require('../lib/Publisher/Narou.js');
 const Note = require('../lib/Publisher/Note.js');
 const Novelabo = require('../lib/Publisher/Novelabo.js');
-const Epub = require('../lib/Publisher/Epub.js');
 
 new Publisher().initDistDir()
 .then( () => new FileReader().read() )
@@ -15,6 +14,5 @@ new Publisher().initDistDir()
     new Narou(contents).publish(),
     new Novelabo(contents).publish(),
     new Note(contents).publish(),
-    new Epub(contents).publish()
 ]) )
 .then( () => console.log('novel-build done.') );
